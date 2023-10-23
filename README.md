8135d6c40a10fce60d7c11be31ccddea01592888
tsosunchia <59512455+tsosunchia@users.noreply.github.com>
2023-10-23 14:02:04 +0800

    init

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# wscat-go
-
-## 运行截图
-<img width="585" alt="image" src="https://github.com/nxtrace/wscat-go/assets/59512455/2a84d483-a1d9-4144-b644-05277f448038">
+# alive-probe
--- main.go
+++ main.go
@@ -18,29 +18,8 @@ import (
 	"github.com/nxtrace/wscat-go/pow"
 )
 
-type IpInfo struct {
-	IP          string  `json:"ip"`
-	AsNumber    string  `json:"asnumber"`
-	Country     string  `json:"country"`
-	CountryEn   string  `json:"country_en"`
-	CountryCode string  `json:"country_code"`
-	Prov        string  `json:"prov"`
-	ProvEn      string  `json:"prov_en"`
-	City        string  `json:"city"`
-	CityEn      string  `json:"city_en"`
-	District    string  `json:"district"`
-	Owner       string  `json:"owner"`
-	ISP         string  `json:"isp"`
-	Domain      string  `json:"domain"`
-	Whois       string  `json:"whois"`
-	Prefix      string  `json:"prefix"`
-	Lat         float64 `json:"lat"`
-	Lng         float64 `json:"lng"`
-	Router      string  `json:"router"`
-	Source      string  `json:"source"`
-}
-
 func main() {
+	fmt.Println(time.Now())
 	var fastIp string
 	host, port := util.GetHostAndPort()
 	// 如果 host 是一个 IP 使用默认域名
@@ -123,40 +102,26 @@ func main() {
 		}
 	}(rl)
 
-	for {
-		line, err := rl.Readline()
-		if err != nil { // io.EOF
-			break
-		}
-
-		err = c.WriteMessage(websocket.TextMessage, []byte(line))
+	flagV4 := false
+	v4IP := util.GetenvDefault("V4IP", "1.1.1.1")
+	for i := 0; i < 3; i++ {
+		err = c.WriteMessage(websocket.TextMessage, []byte(v4IP))
 		if err != nil {
 			fmt.Println("发送失败:", err)
-			break
+			continue
 		}
 
 		_, message, err := c.ReadMessage()
 		if err != nil {
 			fmt.Println("接收失败:", err)
-			break
+			continue
 		}
-
-		// var ipInfo IpInfo
-		// err = json.Unmarshal(message, &ipInfo)
-		// if err != nil {
-		// 	fmt.Println("JSON解析失败:", err)
-		// 	break
-		// }
-
-		// color.Cyan("ip: %s", ipInfo.IP)
-		// color.Green("ASN: %s", ipInfo.AsNumber)
-		// color.Yellow("Geo: %s %s %s", ipInfo.Country, ipInfo.Prov, ipInfo.City)
-		// color.Magenta("Owner: %s", ipInfo.Owner)
 
 		var ipObj map[string]interface{}
 		err = json.Unmarshal(message, &ipObj)
 		if err != nil {
 			fmt.Println("JSON解析失败:", err)
+			continue
 		}
 
 		// New colorjson Formatter
@@ -165,5 +130,41 @@ func main() {
 
 		s, _ := f.Marshal(ipObj)
 		fmt.Println(string(s))
+		flagV4 = true
+		break
+	}
+	flagV6 := false
+	v6IP := util.GetenvDefault("V6IP", "2400:3200::1")
+	for i := 0; i < 3; i++ {
+		err = c.WriteMessage(websocket.TextMessage, []byte(v6IP))
+		if err != nil {
+			fmt.Println("发送失败:", err)
+			continue
+		}
+
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			fmt.Println("接收失败:", err)
+			continue
+		}
+
+		var ipObj map[string]interface{}
+		err = json.Unmarshal(message, &ipObj)
+		if err != nil {
+			fmt.Println("JSON解析失败:", err)
+			continue
+		}
+
+		// New colorjson Formatter
+		f := colorjson.NewFormatter()
+		f.Indent = 2
+
+		s, _ := f.Marshal(ipObj)
+		fmt.Println(string(s))
+		flagV6 = true
+		break
+	}
+	if flagV4 && flagV6 {
+		fmt.Println("#### 存活 ####")
 	}
 }
